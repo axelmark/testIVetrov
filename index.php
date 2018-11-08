d6aa6563c73aef730544c34e932d32a1f62f0675
Axel Mark <axelmark@Axels-MacBook-Pro.local>
2018-11-08 14:09:37 +0300

    tets

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script></script>
     <title>Document</title>
 </head>
 <body>
