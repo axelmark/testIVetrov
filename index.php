58aac49811be17614602c2178aa4922e86a2a73e
Axel Mark <axelmark@Axels-MacBook-Pro.local>
2018-11-11 22:34:33 +0300

    test

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script type="text/javascript">
 
         $(document).ready(function () {
@@ -17,14 +17,15 @@
                 $.ajax({
                     type: 'POST',
                     url: 'script.php',
-                    data:  new FormData(this),
+                    data: new FormData(this),
                     contentType: false,
                     cache: false,
-                    processData:false,
-
+                    processData: false,
 
                     success: function (result) {
-                        alert(result);
+                        $('#my_form').trigger('reset');
+                        $('#info').append(result);
+                        //alert(result);
                     },
                 });
             });
@@ -34,18 +35,14 @@
     <title>Test</title>
 </head>
 <body>
-<?php
-
-?>
 
 <form method="post" id="my_form" enctype="multipart/form-data">
     <input type="file" name="myfile" id="myfile">
     <input type="submit">
 </form>
-<div id="result">
+<div id="info">
 
 </div>
-
 
 </body>
 </html>
