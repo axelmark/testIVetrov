2585745dd890ac2237a1d1caa6f4d79d919061cf
Axel Mark <axelmark@Axels-MacBook-Pro.local>
2018-11-11 22:22:04 +0300

    test

--- index.php
+++ index.php
@@ -5,53 +5,47 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+
     <script type="text/javascript">
-        // $(function () {
-        //     $('#form').submit(function (e) {
-        //         e.preventDefault();
-        //         var data = $(this).serialize();
-        //         $.ajax({
-        //             type: "POST",
-        //             url: "index.php",
-        //             data: data,
-        //             success: function (result) {
-        //                 $('#result').html(result);
-        //             }
-        //         });
-        //     });
+
+        $(document).ready(function () {
+
+            $('#my_form').submit(function (e) {
+                e.preventDefault();
+
+                $.ajax({
+                    type: 'POST',
+                    url: 'script.php',
+                    data:  new FormData(this),
+                    contentType: false,
+                    cache: false,
+                    processData:false,
+
+
+                    success: function (result) {
+                        alert(result);
+                    },
+                });
+            });
+        })
     </script>
-    <title>Document</title>
+
+    <title>Test</title>
 </head>
 <body>
-<form method="post" enctype="multipart/form-data" id="form">
-    <input type="file" name="picture">
-    <input type="submit" value="Загрузить">
+<?php
+
+?>
+
+<form method="post" id="my_form" enctype="multipart/form-data">
+    <input type="file" name="myfile" id="myfile">
+    <input type="submit">
 </form>
 <div id="result">
 
 </div>
-<?php
-// Пути загрузки файлов
-$path = './gallery/';
-$tmp_path = 'tmp/';
-$types = array('image/gif', 'image/png', 'image/jpeg'); // Допустимый тип файла
-$size = 10240000; // Максимальный размер файла
-// Обработка запроса
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Проверяем тип файла
-    if (!in_array($_FILES['picture']['type'], $types))
-        die('Неверный тип файла. <a href="?">Попробовать другой файл?</a>');
-    // Проверяем размер файла
-    if ($_FILES['picture']['size'] > $size)
-        die('Недопустимый размер файла. <a href="?">Попробовать другой файл?</a>');
-    // Загрузка файла и вывод сообщения
-    if (!copy($_FILES['picture']['tmp_name'], $path . $_FILES['picture']['name'])) {
-        echo 'Что-то пошло не так';
-    } else
-        echo 'картинка успешно загружена <a href="gallery.php">Перетий в галерею</a> ';
-}
-?>
+
 
 </body>
 </html>
---
+++ script.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+// Пути загрузки файлов
+$path = 'gallery/';
+$tmp_path = 'tmp/';
+$types = array('image/gif', 'image/png', 'image/jpeg'); // Допустимый тип файла
+//$size = 10240000; // Максимальный размер файла
+
+//Обработка запроса
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Проверяем тип файла
+    if (!in_array($_FILES['myfile']['type'], $types))
+        echo ('Неверный тип файла. Попробовать другой файл?');
+
+    // Проверяем размер файла
+//    if ($_FILES['myfile']['size'] > $size)
+//        die('Недопустимый размер файла. <a href="?">Попробовать другой файл?</a>');
+
+    // Загрузка файла и вывод сообщения
+    if (!copy('' . $_FILES['myfile']['tmp_name'], '' . $path . '' . $_FILES['myfile']['name']))
+        echo 'Что-то пошло не так';
+    else
+        echo 'картинка успешно загружена <a href="gallery.php">Перетий в галерею</a> ';
+}
