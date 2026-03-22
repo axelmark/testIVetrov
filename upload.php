fdef330f7819b55ea2eca17c0c8e8932c6cfbbce
axelmark <axelmark@outlook.com>
2026-03-23 02:41:54 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,16 @@
 
+<?php
+require_once 'classes/ImageManager.php';
+
+header('Content-Type: application/json');
+
+$response = ['success' => false, 'message' => 'Неверный запрос'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
+    $manager = new ImageManager();
+    $response = $manager->upload($_FILES['image']);
+} else {
+    $response['message'] = 'Файл не передан';
+}
+
+echo json_encode($response);
